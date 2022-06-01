6a852977e6f91750e33525b1aaa2fd1d0e051a5d
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-01 12:00:47 -0700

    Update

--- App.js
+++ App.js
@@ -1,140 +1,118 @@
-import React from "react";
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-
-/* export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-} */
-
 export default function App() {
   return (
-    /* Column Settings */
-  <View style={styles.container}>
-    <View style={styles.coulumBox}>
-       <Text style={styles.columnText}>Column</Text>
-    <View style={styles.childBox}>
-      <Text style={styles.boxText}>Child</Text> </View>
-    <View style={styles.childBox}>
-      <Text style={styles.boxText}>Child</Text> </View>
-    <View style={styles.childBox}>
-      <Text style={styles.boxText}>Child</Text></View>
 
-{/* ROW Settings */}
-    <View style={styles.rowBox}> 
-      <Text style={styles.rowText}>Row</Text>
-    <View style={styles.childRow}>
-      <Text style={styles.boxText}>Child</Text></View>
-    <View style={styles.childRow}>
-      <Text style={styles.boxText}>Child</Text></View>
-    <View style={styles.childRow}>
-      <Text style={styles.boxText}>Child</Text></View>
-    </View>
-    </View>
-    </View>
+    // Main outer box to create borders around whole screen
+    <View style={styles.container}>
+      
+      {/*Begin Column box container */}
+      <View style={styles.columnContainer}>
+        <Text style={styles.text}>Column</Text>
+        
+        {/*Column*/}
+        <View style={styles.columnBox}>
+          <Text style={styles.text}>Child</Text></View>
+        
+        <View style={styles.columnBox}>
+          <Text style={styles.text}>Child</Text></View>
+        
+
+        <View style={styles.columnBox}>
+          <Text style={styles.text}>Child</Text></View>
+        
+        </View> {/* close tag for column */} 
+
+      <View style={styles.rowContainer}> 
+      <Text style={styles.text}>Row</Text> 
+      
+      {/*Begin Row box container */}
+      <View style={styles.rowFlexDesign}>
+        
+        {/*Row*/}
+        <View style={styles.rowBox}>
+        <Text style={styles.text}>Child</Text> </View>
+
+        <View style={styles.rowBox}>
+          <Text style={styles.text}>Child</Text></View>
+        
+        <View style={styles.rowBox}>
+          <Text style={styles.text}>Child</Text></View>
+         
+        </View>     
+      </View>   
+      <StatusBar style="auto" />
+
+    </View> /**close view for main container */
     
   );
 }
-/*Container Box */
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E1DBDB',
+    backgroundColor: 'ghostwhite',
+    marginVertical: '10%',
+    marginHorizontal: '5%',
+    borderWidth: 2,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  columnContainer: {
     borderWidth: 2,
-    borderColor: 'green',
-    marginTop: '10%',
-    marginBottom: '10%',
-    marginLeft: '5%',
-    marginRight: '5%',
+    borderColor : 'green',
+    marginVertical: 10,
+    marginLeft: 20,
+    marginRight: 260
+  },
+  rowFlexDesign: {
+    flexDirection: 'row',
     
+  },
+  rowContainer: {
+    
+    borderWidth: 2,
+    borderColor : 'green',
 
   },
-  /*Container Column */
-coulumBox:{
-    width: 150,
-    height: 250,
-    alignItems: 'center',
+  columnBox: {
     borderWidth: 2,
-    borderStyle: 'solid',
-    borderColor: 'blue',
-    marginVertical: '1%',
-    marginLeft: '15%',
-    marginRight: '70%',
-    justifyContent: 'left'
-},
+    backgroundColor: 'lightgray',
+    borderColor : 'green',
+    width: 80,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
+    marginHorizontal: 5
 
-
-/* Coulum Child Text Style */
-childBox: {
+  },
+  rowBox: {
+    borderWidth: 2,
+    backgroundColor: 'lightgray',
+    borderColor : 'green',
     width: 100,
     height: 50,
     alignItems: 'center',
-    borderWidth: 2,
-    borderStyle: 'solid',
-    borderColor: 'blue',
-    marginVertical: '5%',
-    backgroundColor: 'lightgray',
-    justifyContent: 'space-evenly'
-},
-/*Container Row */
-rowBox:{
-  flexDirection: 'row',
-  width: 400,
-  height: 115,
-  alignItems: 'center',
-  borderWidth: 2,
-  marginTop: '50%',
-  borderStyle: 'solid',
-  borderColor: 'red',
-  marginLeft: '170%',
-  justifyContent: 'space-evenly'
-},
-/* Row Child Text Style */
-childRow: {
-  width: 100,
-  height: 50,
-  alignItems: 'center',
-  borderWidth: 2,
-  borderStyle: 'solid',
-  borderColor: 'red',
-  marginVertical: '5%',
-  marginBottom: '1%',
-  backgroundColor: 'lightgray',
-  justifyContent: 'center'
-},
-/** Row Title */
-rowText:{
-  fontSize: 18,
-  color: 'red',
-  fontWeight: 'bold',
-  enum: 'top'
-  /*marginBottom: '20%',
-  marginTop: '1%',
-  justifyContent: 'left'*/ 
-},
+    justifyContent: 'center',
+    marginVertical: 2.5,
+    marginHorizontal: 5
 
-/* child box font*/ 
-boxText:{
-  fontSize: 20,
-  color: 'green',
-  fontWeight: 'bold'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginHorizontal: 5,
+    
+    
+    alignItems: 'center',
 
-},
-/**Colum title */
-columnText:{
-  color:'blue',
-  fontSize: 20,
-  fontWeight: 'bold'
+  },
+  rowHeading: {
+    borderWidth: 2
 
-}
-
-
+  }
 
 });
